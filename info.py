1e3ece004d85f95c2490cdf82d7488e676f692f0
Ayush Sharma <aayush240204@gmail.com>
2020-11-10 22:11:24 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,37 @@
+import re
+from os import environ
+
+# Bot information
+SESSION = environ.get('SESSION', 'Media_search')
+USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot settings
+MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+
+# Admins & Channels
+ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(channel) if re.search('^-100\d+$', channel) else channel for channel in environ['CHANNELS'].split()]
+
+# MongoDB information
+DATABASE_URI = environ['DATABASE_URI']
+DATABASE_NAME = environ['DATABASE_NAME']
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+# Messages
+START_MSG = """
+**Hi I Am Inline-Files-Bot 😍️
+
+Here You Can Search Files/Documents In Inline Mode.
+
+Just Press Following Buttons And Start Searching
+
+Copyright By: CyberBoyAyush 😎️
+
+Here Is The [Source-Code](https://github.com/CyberBoyAyush/Inline-Files-Bot)
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
